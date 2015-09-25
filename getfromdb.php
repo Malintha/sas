7d9312957dcc01fb9ae99aeec41594f0d14de0ce
Malintha Fernando <malintha@ubuntu.ubuntu-domain>
2015-09-26 04:42:15 +0530

    asas

--- getfromdb.php
+++ getfromdb.php
@@ -6,8 +6,19 @@
 	//$msg = $_GET['msg'];
 	$sql = "SELECT * FROM smsstore1 ORDER BY id DESC LIMIT 1";
 	$retval = mysql_query( $sql, $conn );
-	
+
 	echo $retval;
+
+	while($row = mysql_fetch_array($retval, MYSQL_ASSOC))
+{
+    echo "Tutorial ID :{$row['id']}  <br> ".
+         "Title: {$row['name']} <br> ".
+         "Author: {$row['phonenu']} <br> ".
+         "Submission Date : {$row['lon']} <br> ".
+         "--------------------------------<br>";
+} 
+
+echo "Fetched data successfully\n";
 
 
 ?>
