abad5048df4166f42cafb55dd06278fa359eb404
Malintha <malintha@ubuntu.ubuntu-domain>
2015-09-25 23:28:38 +0530

    aaa

--- listener.php
+++ listener.php
@@ -14,7 +14,7 @@ require_once 'lib/SMSSender.php';
 
 define('SERVER_URL', 'http://api.dialog.lk:8080/sms/send');	
 define('APP_ID', 'APP_015755');
-define('APP_PASSWORD', 'password');
+define('APP_PASSWORD', '8aa7ec636c0e6e8acdb91d05da00a747');
 
 $logger = new Logger();
 
