a0693e81b08e11f264a5c66f6b0e59c60a96f4a1
Malintha Fernando <malintha@ubuntu.ubuntu-domain>
2015-09-26 04:34:44 +0530

    asas

--- getfromdb.php
+++ getfromdb.php
@@ -3,7 +3,7 @@
 	require_once 'connectdb.php';
 	
 	//$msg = $_GET['msg'];
-	$sql = "SELECT * FROM smsstore1 ORDER BY id DESC LIMIT 1;"
+	$sql = "SELECT * FROM smsstore1 ORDER BY id DESC LIMIT 1";
 	$result = $conn->query($sql);
 	echo $result;
 	echo 'hello';
