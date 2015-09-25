1caae4ac74406d4f824e9ed96cf738e7451c8a5c
Malintha Fernando <malintha@ubuntu.ubuntu-domain>
2015-09-26 03:03:10 +0530

    asas

--- listener.php
+++ listener.php
@@ -16,16 +16,17 @@ echo 'test1';
     $encoding = $receiver->getEncoding(); // get the encoding value
     $version = $receiver->getVersion(); // get the version
 
-	$sql = "INSERT INTO smsstore (name,phonenu,longitude,latitude) VALUES ('Ramindu','".$address."','".$content."','".$content."')";
+	$sql = "INSERT INTO smsstore (name,phonenu,longitude,latitude) VALUES ('Ramindu','".$address."','".$content."','".$content."')";	
 	if ($conn->query($sql) === TRUE) {
     echo "success";
 	} else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 	}
 
+	if($content=="Sn test"){
 	$sender = new SMSSender( SERVER_URL, APP_ID,  APP_PASSWORD); 
-	$sender->sms( "Ramindu is  being abused!!", $address);
-
+	$response=$sender->sms('This message is sent only to one user', $address);
+}
 
 	echo 'test2';
 
