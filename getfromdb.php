676bebc93f0fb6f8f2fbc6387dc2e54fb621e2e2
Malintha Fernando <malintha@ubuntu.ubuntu-domain>
2015-09-26 04:49:30 +0530

    asas

--- getfromdb.php
+++ getfromdb.php
@@ -8,14 +8,20 @@
 	$retval = mysql_query( $sql, $conn );
 
 	// echo $retval;
-
-	while($row = mysql_fetch_array($retval, MYSQL_ASSOC))
+if(! $retval )
 {
-
-    echo $row['name']; 
+  die('Could not get data: ' . mysql_error());
+}
+while($row = mysql_fetch_assoc($retval))
+{
+    echo "Tutorial ID :{$row['id']}  <br> ".
+         "Title: {$row['name']} <br> ".
+         "Author: {$row['lon']} <br> ".
+         "Submission Date : {$row['lat']} <br> ".
+         "--------------------------------<br>";
 } 
 
-// echo "Fetched data successfully\n";
+echo "Fetched data successfully\n";
 
 
 ?>
