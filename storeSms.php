d5da0ef884f18135c08d5fba37a31d7da6317385
Malintha <malintha@ubuntu.ubuntu-domain>
2015-09-25 22:10:55 +0530

    init

---
+++ storeSms.php
@@ -0,0 +1,22 @@
+<?php 
+
+require_once 'connectdb.php';
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully\n";
+
+$location = $_GET['name'];
+
+echo $location;
+
+$sql = "INSERT INTO getSms (timest,content) VALUES ('232323', 'location')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+?>
